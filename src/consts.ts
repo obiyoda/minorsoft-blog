c058ae893d85130c48b1c56cfb24cd10b5cb1a70
Jared Daines <jared@banzai.org>
2024-05-13 20:53:33 -0600

    Switching to ASTRO

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Software for Your Minor Needs";
+export const SITE_DESCRIPTION =
+  "Welcome to my site where I talk about my journey as a software engineer and the software I build!";
